1f49bd0b50733f9d2a5408cc3708628f54bd4a6a
SidMinecraft <68089579+BasicCorruption@users.noreply.github.com>
2022-03-09 13:50:28 -0500

    Update Version.hx

--- src/commands/Version.hx
+++ src/commands/Version.hx
@@ -1,1 +1,14 @@
 package commands;
+
+import Main.Reference;
+
+class Version {
+	public var name:String;
+
+	public var desc:String;
+
+	public static function trigger():Int {
+		Console.log('<white>BasicTerminal v${Reference.VERSION}</white>');
+		return 0;
+	}
+}
