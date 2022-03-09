9a3cfeff09f99db65cd3f00dc0dc6f0082252e85
SidMinecraft <68089579+BasicCorruption@users.noreply.github.com>
2022-03-09 14:01:52 -0500

    Update Clr.hx

--- src/commands/Clr.hx
+++ src/commands/Clr.hx
@@ -1,1 +1,12 @@
 package commands;
+
+class Clr {
+	public var name:String;
+
+	public var desc:String;
+
+	public static function trigger():Int {
+		Sys.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+		return 0;
+	}
+}
