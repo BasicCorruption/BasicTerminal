10622f4bea38d63185cc614021dc0d9956363a06
Sid <68089579+BasicCorruption@users.noreply.github.com>
2022-04-02 18:39:20 -0400

    Update Neofetch.hx

--- src/commands/Neofetch.hx
+++ src/commands/Neofetch.hx
@@ -5,11 +5,11 @@ class Neofetch {
 		Console.log("<#ffaa00>                                        </>");
 		Console.log("<#ffaa00>                                        </>");
 		Console.log("<#ffaa00>     %%%%%%%%*             (%%%%%%%(    </>   <#ff0000>Haxe: 4.2.5</>");
-		Console.log("<#ffaa00>     &%%%%%%%%%%%%.   %%%((((((((((/    </>   <#ff9900>OS: " + Sys.systemName() + "</>");
+		Console.log("<#ffaa00>     &%%%%%%%%%%%%.   %%%((((((((((/    </>   <#ff9900>OS: " + Sys.environment()["OS"] + "</>");
 		Console.log("<#ffaa00>     &&%%%%%%%%%%%%###((((((((((((//    </>   <#ffff00>User: " + Sys.environment()["USERNAME"] + "</>");
 		Console.log("<#ffaa00>     &&%%%%%%%%%########((((((((((//    </>   <#00ff00>System: " + Sys.environment()["COMPUTERNAME"] + "</>");
-		Console.log("<#ffaa00>      &&%%%%%%#############((((((/      </>");
-		Console.log("<#ffaa00>        %%%##################(((/       </>");
+		Console.log("<#ffaa00>      &&%%%%%%#############((((((/      </>   <#00ffff>Architecture: " + Sys.environment()["PROCESSOR_ARCHITECTURE"] + "</>");
+		Console.log("<#ffaa00>        %%%##################(((/       </>   <#0000ff>Processors: " + Sys.environment()["NUMBER_OF_PROCESSORS"] + "</>");
 		Console.log("<#ffaa00>         #######################        </>");
 		Console.log("<#ffaa00>        #####################(((/       </>");
 		Console.log("<#ffaa00>      &&###################((((((/      </>");
