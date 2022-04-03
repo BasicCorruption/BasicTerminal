a90030b6442c6f8ffc7575051c51fbff4e8a8e4c
Sid <68089579+BasicCorruption@users.noreply.github.com>
2022-04-02 22:29:35 -0400

    Update debug.bat

--- debug.bat
+++ debug.bat
@@ -3,6 +3,7 @@ haxe debug.hxml
 mkdir export
 copy bin\bterm\Main.exe export\BasicTerminal.exe
 copy bin\nsh\Main.exe export\Nsh.exe
+copy bin\fileexec\Main.exe export\FileExec.exe
 echo.
 echo.
 cd export
