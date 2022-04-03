31023cd0e9acd3badb532e63e2de26084bcb3df9
Sid <68089579+BasicCorruption@users.noreply.github.com>
2022-04-03 17:57:59 -0400

    update compiler

--- debug.bat
+++ debug.bat
@@ -1,11 +1,3 @@
-@echo off
-haxe debug.hxml
-mkdir export
-copy bin\bterm\Main.exe export\BasicTerminal.exe
-copy bin\nsh\Main.exe export\Nsh.exe
-copy bin\fileexec\Main.exe export\FileExec.exe
-echo.
-echo.
 cd export
 BasicTerminal
 cd ..
