508ebfad37ae81a8e562bc73663f7e6b4e997fad
Sid <68089579+BasicCorruption@users.noreply.github.com>
2022-04-02 20:20:03 -0400

    stea- write code for detecting haxe version

--- src/commands/Neofetch.hx
+++ src/commands/Neofetch.hx
@@ -4,7 +4,7 @@ class Neofetch {
 	public static function execute(args:Array<String>) {
 		Console.log("<#ffaa00>                                        </>");
 		Console.log("<#ffaa00>                                        </>");
-		Console.log("<#ffaa00>     %%%%%%%%*             (%%%%%%%(    </>   <#ff0000>Haxe: 4.2.5</>");
+		Console.log("<#ffaa00>     %%%%%%%%*             (%%%%%%%(    </>   <#ff0000>Haxe: " + StaticExtender.getHaxeCompilerVersion() + "</>");
 		Console.log("<#ffaa00>     &%%%%%%%%%%%%.   %%%((((((((((/    </>   <#ff9900>OS: " + Sys.environment()["OS"] + "</>");
 		Console.log("<#ffaa00>     &&%%%%%%%%%%%%###((((((((((((//    </>   <#ffff00>User: " + Sys.environment()["USERNAME"] + "</>");
 		Console.log("<#ffaa00>     &&%%%%%%%%%########((((((((((//    </>   <#00ff00>System: " + Sys.environment()["COMPUTERNAME"] + "</>");
@@ -18,5 +18,20 @@ class Neofetch {
 		Console.log("<#ffaa00>     &##########(((   ###((((((((((/    </>");
 		Console.log("<#ffaa00>     #((((((((             .#######(    </>");
 		Console.log("<#ffaa00>                                        </>");
+	}
+}
+
+class StaticExtender {
+	public static macro function getHaxeCompilerVersion():haxe.macro.Expr {
+		var proc_haxe_version = new sys.io.Process('haxe', ['-version']);
+		if (proc_haxe_version.exitCode() != 0) {
+			throw("`haxe -version` failed: " + proc_haxe_version.stderr.readAll().toString());
+		}
+		#if (haxe_ver >= 4)
+		var haxe_ver = proc_haxe_version.stdout.readLine();
+		#else
+		var haxe_ver = proc_haxe_version.stderr.readLine();
+		#end
+		return macro $v{haxe_ver};
 	}
 }
