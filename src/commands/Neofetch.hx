5de30bc6433a4352c9149950ee88fe0e7c8ebeff
BasicCorruption <saytleader@outlook.com>
2022-04-03 20:17:58 -0400

    Add stuff

--- src/commands/Neofetch.hx
+++ src/commands/Neofetch.hx
@@ -4,15 +4,15 @@ class Neofetch {
 	public static function execute(args:Array<String>) {
 		Console.log("<#ffaa00>                                        </>");
 		Console.log("<#ffaa00>                                        </>");
-		Console.log("<#ffaa00>     %%%%%%%%*             (%%%%%%%(    </>   <#ff0000>Haxe: " + StaticExtender.getHaxeCompilerVersion() + "</>");
-		Console.log("<#ffaa00>     &%%%%%%%%%%%%.   %%%((((((((((/    </>   <#ff9900>OS: " + Sys.systemName() + "</>");
-		Console.log("<#ffaa00>     &&%%%%%%%%%%%%###((((((((((((//    </>   <#ffff00>User: " + Sys.environment()["USERNAME"] + "</>");
-		Console.log("<#ffaa00>     &&%%%%%%%%%########((((((((((//    </>   <#00ff00>System: " + Sys.environment()["COMPUTERNAME"] + "</>");
-		Console.log("<#ffaa00>      &&%%%%%%#############((((((/      </>   <#00ffff>Architecture: " + Sys.environment()["PROCESSOR_ARCHITECTURE"] + "</>");
-		Console.log("<#ffaa00>        %%%##################(((/       </>   <#0000ff>Processors: " + Sys.environment()["NUMBER_OF_PROCESSORS"] + "</>");
-		Console.log("<#ffaa00>         #######################        </>");
-		Console.log("<#ffaa00>        #####################(((/       </>");
-		Console.log("<#ffaa00>      &&###################((((((/      </>");
+		Console.log("<#ffaa00>     %%%%%%%%*             (%%%%%%%(    </>   <#000000>Haxe: " + StaticExtender.getHaxeCompilerVersion() + "</>");
+		Console.log("<#ffaa00>     &%%%%%%%%%%%%.   %%%((((((((((/    </>   <#ff0000>OS: " + Sys.systemName() + "</>");
+		Console.log("<#ffaa00>     &&%%%%%%%%%%%%###((((((((((((//    </>   <#ff9900>User: " + Sys.environment()["USERNAME"] + "</>");
+		Console.log("<#ffaa00>     &&%%%%%%%%%########((((((((((//    </>   <#ffff00>System: " + Sys.environment()["COMPUTERNAME"] + "</>");
+		Console.log("<#ffaa00>      &&%%%%%%#############((((((/      </>   <#00ff00>Architecture: " + Sys.environment()["PROCESSOR_ARCHITECTURE"] + "</>");
+		Console.log("<#ffaa00>        %%%##################(((/       </>   <#00ffff>Processors: " + Sys.environment()["NUMBER_OF_PROCESSORS"] + "</>");
+		Console.log("<#ffaa00>         #######################        </>   <#0000ff>Shell: " + Sys.environment()["SHELL"] + "</>");
+		Console.log("<#ffaa00>        #####################(((/       </>   <#8844ee>Desktop: " + Sys.environment()["DESKTOP_SESSION"] + "</>");
+		Console.log("<#ffaa00>      &&###################((((((/      </>   <#ff00ff>Home: " + Sys.environment()["HOME"] + "</>");
 		Console.log("<#ffaa00>     &&#################((((((((((//    </>");
 		Console.log("<#ffaa00>     &&###############((((((((((((//    </>");
 		Console.log("<#ffaa00>     &##########(((   ###((((((((((/    </>");
